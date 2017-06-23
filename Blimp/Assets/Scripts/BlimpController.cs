f84331e52df6ad87de87412bef0dc934e2b31fdf
scottcher <scott.cherkofsky@gmail.com>
2017-06-23 14:03:10 -0400

    Finishing touches for Blimp demo

--- Blimp/Assets/Scripts/BlimpController.cs
+++ Blimp/Assets/Scripts/BlimpController.cs
@@ -6,17 +6,17 @@ public class BlimpController : MonoBehaviour {
 
     float timeCounter;
 
-    [Tooltip("Move speed.")]
+    [Tooltip("Flight speed.")]
     public float Speed = 0.2f;
 
-    [Tooltip("length of the curve to move along.")]
-    public float Length = 4.0f;
+    [Tooltip("Flightpath semi-major radius.")]
+    public float RadiusSemiMajor = 4.0f;
 
-    [Tooltip("width of the curve to move along.")]
-    public float Width = 4.0f;
+    [Tooltip("Flightpath semi-minor radius.")]
+    public float RadiusSemiMinor = 4.0f;
 
     [Tooltip("The center hub around which orbiting occurs.")]
-    GameObject Hub;
+    public GameObject Hub;
 
     Vector3 centerPosition;
     Vector3 lookPosition;
@@ -25,23 +25,25 @@ public class BlimpController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         timeCounter = 0.0f;
-        centerPosition = transform.position;
-	}
+        getHubPosition();
+    }
 	
 	// Update is called once per frame
 	void Update () {
         positionMe();
         rotateMe();
+
+        // its possible the center of rotation, the hub position, has moved, so check and update
+        getHubPosition();
     }
 
-    void positionHub()
+    void getHubPosition()
     {
         // check to make sure the hub was wired up - return early if not
         if (Hub == null) return;
 
-        // the hub is a sibling to the blimp container within the stage so get it and re-position it
-        // to the centerPosition of rotation
-        Hub.transform.position = centerPosition;
+        // its possible the center of rotation, the hub position, has moved, so check and update
+        centerPosition = Hub.transform.position;
     }
 
     // Position change to rotate around a center position
@@ -49,8 +51,8 @@ public class BlimpController : MonoBehaviour {
     {
         timeCounter += Time.deltaTime * Speed;
         
-        float x = Mathf.Cos(timeCounter) * Width + centerPosition.x;
-        float z = Mathf.Sin(timeCounter) * Length + centerPosition.z;
+        float x = Mathf.Cos(timeCounter) * RadiusSemiMinor + centerPosition.x;
+        float z = Mathf.Sin(timeCounter) * RadiusSemiMajor + centerPosition.z;
         float y = 0.0f + centerPosition.y;
 
         transform.position = new Vector3(x, y, z);
